0ebd72186dca5edf950726c37208a8543c6db9e2
Alexander Nye <alexnye@outlook.com>
2020-09-25 21:01:23 -0700

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use rocket_contrib::serve::StaticFiles;
 
 #[get("/forums")]
 fn forums() -> &'static str {
-	"Hello, world!"
+    "Hello, world!"
 }
 
 fn main() {
-	rocket::ignite()
-      .mount("/", StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")))
-      .mount("/forums", routes![forums]).launch();
+    rocket::ignite()
+        .mount(
+            "/",
+            StaticFiles::from(concat!(env!("CARGO_MANIFEST_DIR"), "/static")),
+        )
+        .mount("/forums", routes![forums])
+        .launch();
 }
