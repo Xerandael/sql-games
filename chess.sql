0c9440d597f47192378ddb3c74d6a0b07d5c92de
Alexander Nye <alexnye@outlook.com>
2020-11-28 11:01:00 -0800

    checkpoint

--- chess.sql
+++ chess.sql
@@ -1,3 +1,4 @@
+-- TODO: move to file `definitions.sql` -- also add schema prefixes?  `definitions.natural_numbers`
 create view natural_numbers as (
   with recursive natural_numbers as (
     select 1 as n
@@ -84,6 +85,7 @@ create materialized view initial_board as (
   join board on squares.square_num = board.n
 );
 
+
 create view piece_movements as (
   with starting_positions as (
     select x as start_x, y as start_y, m.n as start_w, n.n as start_z
@@ -103,9 +105,13 @@ create view piece_movements as (
       false as attacking,
       'p' as sym
     from starting_positions
-    cross join natural_numbers n -- TODO: use in arithmetic condition for moving 2 squares
-    cross join (values (1), (-1)) as piece_color -- TODO use in multiplication?
-    cross join directions -- TODO?
+    cross join
+      (values (1), (-1)) as piece_color
+    join lines
+      on  (y = piece_color)
+      and (steps_taken in (1, 2))
+      and (((end_x - start_x) in (-1, 0, 1)) or ((end_w - start_w) in (-1, 0, 1)))
+      and (() * ()) -- TODO
   ),
   rook as (
     -- TODO
@@ -130,18 +136,14 @@ create view piece_movements as (
   union select * from king
 );
 
+-- TODO: move to file `events.sql`
 create sequence game_seq;
 
---------------------------------------------------------------------------------------------------------------------------------
-
 create table moves (
-  -------------------------------------------------------------------------------
-  -- base columns
-  -------------------------------------------------------------------------------
   game            int not null default nextval('game_seq'),
   to_timeline     int not null,
   from_timeline   int not null,
-  real_turn       int not null check (real_turn > 0), -- mostly used as a way of tracking the order moves were made in in actual time
+  order           int not null check (order > 0), -- mostly used as a way of tracking the order moves were made in in actual time
   to_turn         int not null check (to_turn > 0),
   from_turn       int not null,
   to_x            int not null,
@@ -149,92 +151,82 @@ create table moves (
   to_y            int not null,
   from_y          int not null,
 
-  -------------------------------------------------------------------------------
-  -- generated columns
-  -------------------------------------------------------------------------------
-  prev_turn       int generated always as (nullif((real_turn - 1), 0)) stored,
+  prev_turn       int generated always as (nullif((order - 1), 0)) stored,
   -- black is 1 and white is -1 the purposes of pawn movements as well as timelines
   player          int generated always as (((from_turn % 2) * 2) - 1) stored,
 
-  -------------------------------------------------------------------------------
-  -- constraints
-  -------------------------------------------------------------------------------
   -- you have to actually move
   check ((from_timeline != to_timeline) or (from_turn != to_turn) or (from_x != to_x) or (from_y != to_y)),
   -- validate sequentiality of real turns starting at turn 1 each game
-  unique(game, real_turn),
-  foreign key (game,prev_turn) references moves(game,real_turn),
+  unique(game, order),
+  foreign key (game,prev_turn) references moves(game,order),
   -- can only move to boards of own color
   check ((from_turn % 2) = (to_turn % 2))
 
-  -------------------------------------------------------------------------------
-  -- indexes
-  -------------------------------------------------------------------------------
 );
--- castling is handled by just recording the king's move and automatically moving the rook thereafter
-create unique index can_only_move_once_per_board_turn on moves(game, from_timeline, from_turn);
-create index starting_location on moves(game, from_timeline, from_turn, from_x, from_y);
-create index ending_location on moves(game, to_timeline, to_turn, to_x, to_y);
-create index real_turns on moves(game,real_turn);
--- index to specify the semantics of timeline forking.  Support checking whether a board has been moved to twice.
-create index ending_board_order on moves(game, to_timeline, to_turn, real_turn);
+create unique index  can_only_move_once_per_board_turn           on moves(game, from_timeline, from_turn);
+create        index  starting_location                           on moves(game, from_timeline, from_turn, from_x, from_y);
+create        index  ending_location                             on moves(game, to_timeline, to_turn, to_x, to_y);
+create unique index  order                                       on moves(game, order);
+create        index  ordered_list_of_moves_onto_any_given_board  on moves(game, to_timeline, to_turn, order);
 
---------------------------------------------------------------------------------------------------------------------------------
-
-create view state as (
-  with recursive state as (
-    -- TODO: how get the initial board in here?
-    with board_events as (
-      -- TODO: I think the moving player has to be determined by the color of the piece being moved
-      with previous_state as (
-        -- TODO: unpack
-      ),
-      new_state as (
-        -- TODO: compute
-        -- TODO: `moved_to_by, moved_to_at`
-      )
-      select * from old_state union all select * from new_state
+-- TODO: move to `computation.sql`
+create view state as ( with recursive state as (
+  -- TODO: how get the initial board in here?
+  with board_events as (
+    -- TODO: I think the moving player has to be determined by the color of the piece being moved
+    with previous_state as (
+      -- TODO: unpack
     ),
-    piece_positions as (
-      with previous_state as (
-        -- TODO: unpack
-      ),
-      new_state as (
-        -- TODO: compute
-      )
-      select * from old_state union all select * from new_state
-    ),
-    available_moves as (
-      with previous_state as (
-        -- TODO: unpack
-      ),
-      new_state as (
-        -- TODO: compute
-        -- TODO:  join positions of moveable pieces against piece movements
-        -- TODO: check for board edges, then check for pieces between the lines cast from the starting position.  Allow ending on an enemy but not an ally.
-        -- join against the set of all pieces on whether they're linearly-between the start and end
-      )
-      select * from old_state union all select * from new_state
-    ),
-    timelines as (
-      with previous_state as (
-        -- TODO: unpack
-      ),
-      new_state as (
-        -- TODO: compute
-      )
-      select * from old_state union all select * from new_state
-    ),
-    pack as (
-      select *
-      from board_events
-      full join available_moves on 0 = 1
-      full join timelines       on 0 = 1
-      full join piece_positions on 0 = 1
+    new_state as (
+      -- TODO: compute
+      -- TODO: `moved_to_by, moved_to_at`
     )
+    select * from old_state union all select * from new_state
+  ),
+  piece_positions as (
+    with previous_state as (
+      -- TODO: unpack
+    ),
+    new_state as (
+      -- TODO: compute
+    )
+    select * from old_state union all select * from new_state
+  ),
+  available_moves as (
+    with previous_state as (
+      -- TODO: unpack
+    ),
+    new_state as (
+      -- TODO: compute
+      -- TODO:  join positions of moveable pieces against piece movements
+      -- TODO: check for board edges, then check for pieces between the lines cast from the starting position.  Allow ending on an enemy but not an ally.
+      -- join against the set of all pieces on whether they're linearly-between the start and end
+    )
+    select * from old_state union all select * from new_state
+  ),
+  timelines as (
+    with previous_state as (
+      -- TODO: unpack
+    ),
+    new_state as (
+      -- TODO: compute
+    )
+    select * from old_state union all select * from new_state
+  ),
+  pack as (
     select *
-    from pack
-  )
-  select *
-  from state
-);
+    from board_events
+    full join available_moves
+      on (
+         select exists (select * from check_state where turn >= present)
+         )
+    full join timelines
+      on (
+         select exists (select * from check_state where turn >= present)
+         )
+    full join piece_positions
+      on (
+         select exists (select * from check_state where turn >= present)
+         )
+) select * from pack) select * from state);
